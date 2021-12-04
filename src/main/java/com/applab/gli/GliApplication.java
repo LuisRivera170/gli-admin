dd9f2c1c1f4f579146b51b358bd94d29710f06b6
Luis Rivera <luis.rivera@infotec.mx>
2021-12-03 19:50:47 -0600

    Commit inicial

---
+++ src/main/java/com/applab/gli/GliApplication.java
@@ -0,0 +1,13 @@
+package com.applab.gli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/applab/gli/GliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applab.gli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
